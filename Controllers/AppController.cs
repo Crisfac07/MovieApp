eac7fce7ab3dca0ef40493756bbcd2c1b592c956
Cris Franco <francochavezcristian@gmail.com>
2022-07-29 14:45:18 -0500

    Modificación de controlador para agregar y color de los botones de la vista Index

--- Controllers/AppController.cs
+++ Controllers/AppController.cs
@@ -3,14 +3,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MovieApp.Models;
 
 namespace MovieApp.Controllers
 {
     public class AppController : Controller
     {
+        private static List<MovieViewModel> _movie = new List<MovieViewModel>(); 
         public IActionResult Index()
         {
-            return View();
+            return View(_movie);
         }
 
         public IActionResult AddOrEdit()
@@ -19,8 +21,14 @@ namespace MovieApp.Controllers
         }
 
         [HttpPost]
-        public IActionResult AddOrEdit(Guid id)
+        public IActionResult AddOrEdit(MovieViewModel model)
         {
+            if (ModelState.IsValid)
+            {
+                model.Id = Guid.NewGuid();
+                _movie.Add(model);
+                return RedirectToAction(nameof(Index));
+            }
             return View();
         }
     }
