b713290e825bd74359a60137c290529f56b25d0b
Cris Franco <francochavezcristian@gmail.com>
2022-07-29 13:52:25 -0500

    Configuración de midelware UseEndpoints

--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,8 @@ namespace MovieApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,15 +27,17 @@ namespace MovieApp
             {
                 app.UseDeveloperExceptionPage();
             }
+            
 
             app.UseRouting();
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapRazorPages();
+                endpoints.MapAreaControllerRoute(
+                    name:"Default",
+                    pattern:"{controller}/{action}/{id?}",
+                    defaults: new {controller="App", action="Index" }
+                    );
             });
         }
     }
